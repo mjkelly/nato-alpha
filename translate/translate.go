539b630d0016b5e49bb05d77deb053b7cde39c3d
Michael Kelly <michael@michaelkelly.org>
2013-10-09 13:23:25 -0400

    Add Google Analytics.

--- translate/translate.go
+++ translate/translate.go
@@ -14,6 +14,14 @@ var mainString = `
 	<title>Phonetic Alphabet: {{.Phrase}}</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-20919137-2', 'nato-alpha.appspot.com');
+		ga('send', 'pageview');
+	</script>
 </head>
 <body>
 	<div class="container">
